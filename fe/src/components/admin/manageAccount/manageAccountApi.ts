001caa236c7f71430cce44a8d6ef5ea27b6ab41e
longldm <longledangminh@gmail.com>
2024-12-16 03:56:16 +0000

    code api for account management manageAccountApi.ts

---
+++ fe/src/components/admin/manageAccount/manageAccountApi.ts
@@ -0,0 +1,57 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { IAccount, ICreateAccountPayload, ICreateAccountResponse, IUpdateAccountPayload, IUpdateAccountResponse, IDeleteAccountPayload, IDeleteAccountResponse } from "../../../types/Account";
+import { sendRequest } from "../../../utils/sendRequest";
+
+export const getAllAccounts = createAsyncThunk<IAccount[]>(
+    'api/get-all-accounts',
+    async (thunkApi) => {
+        return await sendRequest(
+            "/accounts",
+            {
+                method: "GET",
+                thunkApi,
+            }
+        );
+    }
+);
+
+export const createAccount = createAsyncThunk<ICreateAccountResponse, ICreateAccountPayload>(
+    'api/create-account',
+    async (payload: ICreateAccountPayload, thunkApi) => {
+        return await sendRequest(
+            "/accounts",
+            {
+                payload: payload,
+                method: "POST",
+                thunkApi,
+            }
+        );
+    }
+);
+
+export const updateAccount = createAsyncThunk<IUpdateAccountResponse, IUpdateAccountPayload>(
+    'api/update-account',
+    async (payload: IUpdateAccountPayload, thunkApi) => {
+        return await sendRequest(
+            `/accounts/${payload.id}`,
+            {
+                payload: payload,
+                method: "PUT",
+                thunkApi,
+            }
+        );
+    }
+);
+
+export const deleteAccount = createAsyncThunk<IDeleteAccountResponse, IDeleteAccountPayload>(
+    'api/delete-account',
+    async (payload: IDeleteAccountPayload, thunkApi) => {
+        return await sendRequest(
+            `/accounts/${payload.id}`,
+            {
+                method: "DELETE",
+                thunkApi,
+            }
+        );
+    }
+);
